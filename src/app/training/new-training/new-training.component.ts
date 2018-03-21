b0e2049f41472ba2acf311e84a679634baf86204
Zakir Nuriev <zakir.nuriev@caspio.com>
2018-03-21 16:51:29 +0200

    Update New Training component data fetching.

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -4,7 +4,8 @@ import { Exercise } from '../training/exercise.model';
 import { MatSelect } from '@angular/material';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore } from 'angularfire2/firestore';
-import { Observable } from 'rxjs';
+import { Observable, pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-new-training',
@@ -12,7 +13,7 @@ import { Observable } from 'rxjs';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
-  exercises: Observable<any>;
+  exercises: Observable<Exercise[]>;
 
   constructor(private trainingService: TrainingService,
               private db: AngularFirestore) { }
@@ -20,7 +21,20 @@ export class NewTrainingComponent implements OnInit {
   ngOnInit() {
     this.exercises = this.db
       .collection('availableExercises')
-      .valueChanges();
+      .snapshotChanges()
+      .pipe(
+        map(docArray => {
+          return docArray.map(doc => {
+            const data = doc.payload.doc.data();
+            return {
+              id: doc.payload.doc.id,
+              name: data.name,
+              duration: data.duration,
+              calories: data.calories
+            };
+          })
+        })
+      );
   }
 
   onStartTraining(form: NgForm) {
